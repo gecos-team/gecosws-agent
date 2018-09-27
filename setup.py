3778baf5a9eb53a7f45f19b6a46c7eb65d91d0eb
Alfonso E.M <alfonso@el-magnifico.org>
2018-09-27 12:40:27 +0200

    New version with debug mode

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 import glob
 
 setup(name='gecos-agent',
-      version='1.2',
+      version='1.3',
       description='GECOS Agent for Workstations',
       author='Alfonso de Cala',
       author_email='alfonso.cala@juntadeandalucia.es',
